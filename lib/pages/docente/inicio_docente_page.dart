ef67e30a7f343d195e24c1ac115ab5aa33e0831b
Xavito84 <xaviermartinezgarrido@hotmail.com>
2024-05-02 00:24:18 +0200

    Actualizacion 1.0

---
+++ lib/pages/docente/inicio_docente_page.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_inicio/pages/menu/ajustes_page.dart';
+import 'package:proyecto_inicio/pages/menu/eventos_page.dart';
+import 'package:proyecto_inicio/pages/menu/informacion_page.dart';
+
+import '../../main.dart';
+
+void main() {
+  runApp(const InicioDocente());
+}
+
+class InicioDocente extends StatelessWidget {
+  const InicioDocente({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.white, // Color del fondo del Scaffold en blanco
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Docente1'), // Cambia el título a "Docente1"
+          centerTitle: true, // Centra el título en la AppBar
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu), // Icono de las tres rayas
+                onPressed: () {
+                  Scaffold.of(context).openDrawer(); // Abre el Drawer al presionar el icono
+                },
+              );
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add), // Icono de agregar usuario
+              onPressed: () {
+                // Acción al presionar el botón de agregar usuario
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 0), // Espacio desde la parte superior
+              Text(
+                'Selecciona una clase', // Texto principal
+                style: TextStyle(
+                  fontSize: 24.0, // Tamaño del texto más grande
+                  fontWeight: FontWeight.bold, // Peso de la fuente en negrita
+                  color: Colors.black, // Color del texto
+                ),
+              ),
+              SizedBox(height: 50), // Espacio entre el texto y el primer botón
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para el botón P0
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrangeAccent), // Cambia el color de fondo del botón a naranja
+                ),
+                child: SizedBox(
+                  width: 100, // Ancho del botón
+                  child: Center(
+                    child: Text(
+                      'P0', // Texto del botón
+                      style: TextStyle(
+                        color: Colors.black, // Cambia el color del texto a negro
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40), // Espacio entre el primer y segundo botón
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para el botón P1
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrangeAccent), // Cambia el color de fondo del botón a verde
+                ),
+                child: SizedBox(
+                  width: 100, // Ancho del botón
+                  child: Center(
+                    child: Text(
+                      'P0', // Texto del botón
+                      style: TextStyle(
+                        color: Colors.black, // Cambia el color del texto a negro
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40), // Espacio entre el segundo y tercer botón
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para el botón P2
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrangeAccent), // Cambia el color de fondo del botón a verde
+                ),
+                child: SizedBox(
+                  width: 100, // Ancho del botón
+                  child: Center(
+                    child: Text(
+                      'P0', // Texto del botón
+                      style: TextStyle(
+                        color: Colors.black, // Cambia el color del texto a negro
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          // Contenido del menú deslizable
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.brown, // Color de fondo marrón
+                ),
+                child: const Text(
+                  'Menú de Usuario',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Nombre de usuario', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  // Agregar aquí la funcionalidad para el nombre de usuario
+                },
+              ),
+              ListTile(
+                title: const Text('Email', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  // Agregar aquí la funcionalidad para el email
+                },
+              ),
+              ListTile(
+                title: const Text('Eventos', style: TextStyle(color: Colors.white)),
+                onTap: () {
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Eventos()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Información', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Informacion()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Ajustes', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Ajustes()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Salir', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyApp()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login1_page.dart
+++ lib/pages/login1_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_inicio/pages/docente/inicio_docente_page.dart';
 import 'package:proyecto_inicio/pages/recuperar_page.dart';
 
-import 'inicio_alumno_padres_page.dart'; // Importa la página recuperar_page.dart
+import 'inicio_alumno_padres_page.dart'; // Importa la página InicioAlumnoPadresPage
+
 
 class Login1 extends StatelessWidget {
   const Login1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
@@ -70,6 +75,7 @@ class Login1 extends StatelessWidget {
                     ),
                     SizedBox(height: 32.0),
                     TextField(
+                      controller: emailController,
                       decoration: InputDecoration(
                         hintText: 'E-mail', // Texto sugerido para la primera caja de texto
                         hintStyle: TextStyle(color: Colors.grey[400]), // Cambia el color del texto sugerido a gris claro
@@ -77,6 +83,7 @@ class Login1 extends StatelessWidget {
                     ),
                     SizedBox(height: 24.0),
                     TextField(
+                      controller: passwordController,
                       decoration: InputDecoration(
                         hintText: 'Contraseña', // Texto sugerido para la segunda caja de texto
                         hintStyle: TextStyle(color: Colors.grey[400]), // Cambia el color del texto sugerido a gris claro
@@ -86,11 +93,48 @@ class Login1 extends StatelessWidget {
                     SizedBox(height: 80.0),
                     ElevatedButton(
                       onPressed: () {
-                        // Navegar a la ventana InicioAlumno
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => InicioAlumno()),
-                        );
+                        String email = emailController.text;
+                        String password = passwordController.text;
+
+                        if (email.isEmpty) {
+                          // Si el campo de correo electrónico está vacío, muestra un diálogo
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("Error"),
+                                content: Text("El campo de correo electrónico no puede estar vacío."),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context); // Cierra el diálogo
+                                    },
+                                    child: Text("OK"),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        } else if (email == "docente" && password.isEmpty) {
+                          // Si el correo electrónico es "docente" y la contraseña está vacía, navega a la ventana InicioDocente
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => InicioDocente()),
+                          );
+                        } else if (email == "padre" && password.isEmpty) {
+                          // Si el correo electrónico es "padre" y la contraseña está vacía, navega a la ventana InicioPadres
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => InicioAlumno()),
+                          );
+                        } else {
+                          // En otros casos, puedes agregar alguna lógica adicional o mostrar un mensaje de error
+                          // Por ejemplo, podrías mostrar un diálogo indicando que las credenciales son incorrectas
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => InicioAlumno()),
+                          );
+                        }
                       },
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all<Color>(Colors.red), // Cambia el color del botón a rojo
